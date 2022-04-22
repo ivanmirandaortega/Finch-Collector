f3a3480d5ff1f1c891de5ff929de3a1fd4814686
Ivan Miranda Ortega <ivanmirandaortega@Ivans-MBP.lan>
2022-04-22 11:33:18 -0700

    initial commit

---
+++ main_app/views.py
@@ -0,0 +1,13 @@
+import imp
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('<h1>Welcome</h1>')
+
+
+def about(request):
+    return render(request, 'about.html')
